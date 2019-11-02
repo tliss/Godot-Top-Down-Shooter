b0dec1f3122081a452dcad85502db2a3fe843580
Taylor Liss <tayloraliss@gmail.com>
2019-11-02 12:28:07 -0400

    Randomly generate uzis throughout level

--- World.gd
+++ World.gd
@@ -4,24 +4,36 @@ export (PackedScene) var Mob
 var score
 const PICKUP = preload("Pickup.tscn")
 
+var rng = RandomNumberGenerator.new()
+
 func _ready():
 	randomize()
+	rng.randomize()
 	
 func _on_MobTimer_timeout():
 	$MobPath/MobSpawnLocation.set_offset(randi())
 	var mob = Mob.instance()
 	add_child(mob)
-
 	mob.position = $MobPath/MobSpawnLocation.position
-
 	var direction = $MobPath/MobSpawnLocation.rotation + PI / 2
 	mob.rotation = direction
-
 	mob.connect("add_score", $HUD, "update_score")
-	pass
-
+	
 func _on_PickupTimer_timeout():
 	var pickup = PICKUP.instance()
 	add_child(pickup)
-	pickup.position = (Vector2(800, 300))
+
+	var sprite = pickup.get_node("Area2D/Sprite")
+	var sprite_size_x = sprite.texture.get_size().x * sprite.scale.x
+	var sprite_size_y = sprite.texture.get_size().y * sprite.scale.y
+	var pickup_size = Vector2(ceil(sprite_size_x), ceil(sprite_size_y))
+	
+	var location = Vector2()
+	location.x = rng.randi_range(pickup_size.x / 2, 2560 - (pickup_size.x / 2))
+	location.y = rng.randi_range(pickup_size.y / 2, 2560 - (pickup_size.y / 2))
+	
+	print(location)
+	
+	pickup.position = location
+
 	pickup.connect("uzi_pickup", $HUD, "add_weapon")
